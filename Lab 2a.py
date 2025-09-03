5aabb85183cff457f961da7231204e06f88ae335
victor Odonnell <39573414+vipereaper@users.noreply.github.com>
2025-09-02 20:53:13 -0700

    lab2a

---
+++ Lab 2a.py
@@ -0,0 +1,21 @@
+#a
+print ("All the world's a stage")
+print ("And all the men and women merely players:")
+print ("They have their exits and their entrances")
+print ("And one man in his time plays many parts")
+print ("His acts being seven ages")
+
+#b
+print ("All the world's a stage, And all the men and women merely players: They have their exits and their entrances; And one man in his time plays many parts, His acts being seven ages.")
+
+#c
+#127.0.0.1
+
+#d
+# flush=True: Immediately see output as it’s printed, without waiting for buffer.
+# flush=False: Program ends normally or buffering doesn’t matter, printing the final output of a calculation.
+
+#e
+#Using end="\n" makes print flush the output automatically.
+
+# I had a lot of difficulty understanding what flush meant and even more so how it can be used
